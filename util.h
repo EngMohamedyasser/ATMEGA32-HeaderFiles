f9f13ed0909d92ea1844479f89acff564bacc4c8
mohamed shehab <32803176+EngMohamedyasser@users.noreply.github.com>
2022-08-01 18:36:30 -0500

    Update util.h

--- util.h
+++ util.h
@@ -14,15 +14,15 @@ Embedded software engineer at Roots Solutions
 #define CLR_REG(Reg)                  Reg =0x00
 #define TOGGLE_REG(Reg)               Reg ^=0xff
 #define ASSIGN_REG(Reg,value)         Reg = value 
-#define GET_LOW_NIB(Reg)              Reg & 0x0f  
-#define CLR_LOW_NIB(Reg)              Reg &=0xf0;
-#define SET_LOW_NIB(Reg)              Reg |=0x0f;
-#define TOGGLE_LOW_NIB(Reg)           Reg ^= 0x0f 
+#define GET_LOW_NIB(Reg)              Reg & 0xF0  
+#define CLR_LOW_NIB(Reg)              Reg &=0xF0;
+#define SET_LOW_NIB(Reg)              Reg |=0xF0;
+#define TOGGLE_LOW_NIB(Reg)           Reg ^= 0xF0 
 #define ASSIGN_LOW_NIB(Reg,value)     Reg= ((Reg &0xf0)|(value & 0x0f))    // need it more optimized
-#define GET_HIGH_NIB(Reg)            ((Reg & 0xf0) >> 4 )
-#define CLR_HIGH_NIB(Reg)             Reg &= 0x0f 
-#define SET_HIGH_NIB(Reg)             Reg |= 0xf0 
-#define TOGGLE_HIGH_NIB(Reg)          Reg ^= 0xf0 
-#define ASSIGN_HIGH_NIB(Reg,value)    Reg= ((Reg &0x0f)|( value << 4))
+#define GET_HIGH_NIB(Reg)            ((Reg & 0x0F) >> 4 )
+#define CLR_HIGH_NIB(Reg)             Reg &= 0x0F 
+#define SET_HIGH_NIB(Reg)             Reg |= 0x0F 
+#define TOGGLE_HIGH_NIB(Reg)          Reg ^= 0x0F 
+#define ASSIGN_HIGH_NIB(Reg,value)    Reg= ((Reg &0x0F)|( value << 4))
 
 #endif
